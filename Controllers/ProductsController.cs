f4ca4a8c0a9547c63fa1b21333f69a8d4871c651
Rawan Khaled <67297151+rere-kh1999@users.noreply.github.com>
2021-12-27 23:18:36 +0200

    Add/Edit/Delete Products APIs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -52,15 +52,92 @@ namespace MarketplaceAPI.Controllers
             }
             return Ok(product);
         }
+       
+    
+            //Delete a product from the Product's list
+            [HttpDelete("{id:int}")]
+            public IActionResult DeleteProduct(int id)
+            {
+                var instance = _db.Product.Find(id);
+
+                if (instance == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    _db.Remove(instance);
+                    _db.SaveChanges();
+                    var Products = _db.Product.ToList();
+
+                    return Ok(Products);
+                }
+            }
+
+            [HttpPost]
+            public IActionResult AddProduct(ProductModel obj)
+            {
+                // string User_Email = HttpContext.Session.GetString("User_Email");
+                // int User_id = _db.User.Where(p => p.Email == User_Email).Select(o => o.Id).FirstOrDefault();
+
+                Product p = new Product();
+                p.Name = obj.Name;
+                p.Price = obj.Price;
+                p.Quantity = obj.Quantity;
+                p.Category = obj.Category;
+                p.Description = obj.Description;
+                // p.User_Id = _db.User.Where(p => p.Id == User_id).Select(o => o.Id).FirstOrDefault();
+                p.User_Id = 2;
+                p.Id = obj.Id;
+                p.Image = "https://www.lg.com/lg5-common/images/common/product-default-list-350.jpg";
+
+                if (ModelState.IsValid)
+                {
+                    _db.Add(p);
+                    _db.SaveChanges();
+                    var product = _db.Product.Find(p.Id);
+                    return Ok(product);
+                }
+                return Ok("Added product");
+            }
 
 
-/*        [HttpPost]
-        public IActionResult CreateProduct([FromBody]Product product)
-        {
-            _db.Product.Add(product);
-            _db.SaveChanges();
-            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
-        }*/
+            //Edit information of spasciffic product
+            [HttpPut("update-product-by-id/{id}")]
+            public IActionResult EditProduct(int id, ProductModel Up_pro)
+            {
+                if (_db.Product.Find(id) == null)
+                    return Ok("object Not Found");
+                else
+                {
+                    //GetProduct(id);
+                    var obj = _db.Product.Find(id);
+                    // Product p = new Product();
+                    obj.Name = Up_pro.Name;
+                    obj.Price = Up_pro.Price;
+                    obj.Quantity = Up_pro.Quantity;
+                    obj.Category = Up_pro.Category;
+                    obj.Description = Up_pro.Description;
+                    obj.Id = id;
+                    // p.User_Id = obj.User_Id;
+                    if (ModelState.IsValid)
+                    {
+                        //   _db.Update(p);
+                        _db.SaveChanges();
+                        return Ok(obj);
+                    }
+                }
+                return Ok("Not Updated");
+            }
+ 
 
-    }
+    /*        [HttpPost]
+            public IActionResult CreateProduct([FromBody]Product product)
+            {
+                _db.Product.Add(product);
+                _db.SaveChanges();
+                return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+            }*/
+
+}
 }
---
+++ Models/ProductModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MarketplaceAPI.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        //   public string Image { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        //public int? User_Id { get; set; }
+        //[ForeignKey("User_Id")]
+      //   public int? SecondaryUser { get; set; }
+        //[ForeignKey("SecondaryUser")]
+        // [JsonIgnore]
+        //public virtual User User { get; set; }
+        //public int Status { get; set; }
+    }
+}
